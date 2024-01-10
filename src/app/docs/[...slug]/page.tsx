bda6666f09053a973256e61f9977f43d33e5e670
Ghada Nasri <nasrighada10@outlook.com>
2024-01-10 11:13:40 +0100

    second commit

--- src/app/docs/[...slug]/page.tsx
+++ src/app/docs/[...slug]/page.tsx
@@ -1,6 +1,9 @@
-
-export default function Docs() {
-  return (
-    <div>Docs home page</div>
-  )
+export default function Docs({ params }: { params: { slug: string[] } }) {
+  if (params.slug.length === 2) {
+    return <h1>Viewing docs for feature {params.slug[0]} and concept {params.slug[1]}</h1>
+  } else if (params.slug.length === 1) {
+    return <h1>Viewing docs for feature { params.slug[0]}</h1>;
+  }
+  return <h1>Docs home page</h1>
 }
+// add two brackets to the file and ? to the code to see docs home page
