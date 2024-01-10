1bf9f84ea308875fd99af931b3282449c63468f6
Ghada Nasri <nasrighada10@outlook.com>
2024-01-10 12:53:26 +0100

    fifth commit

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,8 +1,13 @@
+import { notFound } from "next/navigation";
+
 export default function ReviewId({
   params,
 }: {
   params: { productId: string; reviewId: string };
-}) {
+  }) {
+  if (parseInt(params.reviewId) > 1000) {
+    notFound()
+  }
   return (
     <div>
       <h1>
